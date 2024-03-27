4a6a8a418b099cc8e0238b2592a9aa29500d0049
Gregor <albadylic@gmail.com>
2024-03-27 14:46:24 +0000

    Basic Select

--- sql/basic-selects.sql
+++ sql/basic-selects.sql
@@ -87,3 +87,39 @@ SELECT DISTINCT CITY FROM STATION WHERE NOT REGEXP_LIKE(CITY,'^[AEIOU]');
 -- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
 
 SELECT DISTINCT CITY FROM STATION WHERE NOT REGEXP_LIKE(CITY,'[aeiou]$');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE NOT REGEXP_LIKE(CITY,'^[AEIOU]') OR NOT REGEXP_LIKE(CITY,'[aeiou]$');
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE NOT REGEXP_LIKE(CITY,'^[AEIOU]') AND NOT REGEXP_LIKE(CITY,'[aeiou]$');
+
+/* Higher Than 75 Marks 
+
+Query the Name of any student in STUDENTS who scored higher than 75 Marks.
+Order your output by the last three characters of each name.
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.),
+secondary sort them by ascending ID.
+*/
+
+SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY SUBSTR(NAME, -3), ID;
+
+/* Employee Names
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+*/
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME;
+
+/* Employee Salaries
+
+Write a query that prints a list of employee names (i.e.: the name attribute)
+for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months.
+Sort your result by ascending employee_id.
+
+*/
+
+SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID;
